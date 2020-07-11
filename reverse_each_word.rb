9840fb02552ff7895fa88d203e29deddd88ea3dd
Denise Henderson <denise@henderson3.us>
2020-07-11 10:44:54 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,23 +3,17 @@ require 'pry'
 def reverse_each_word(string)
   newstring = string.split(" ") #separates string by words
   final_string_array = []
-  arraycount = 0
+  array_count = 0
   newstring.each do |word|   #do for each word in array
-    word_to_rev = newstring[arraycount] #word currently operating on
-    rev_word= []       #empty array to hold reversed string
-    #word_to_reverse << newstring[arraycount].split("") #fill array with letters vs string
-    #begin = 1
-    letterscount = newstring.length
-    #word_to_reverse.each do |char|   #count letters to reverse
-    #  word_to_reverse_array_count += 1
-    #end
-    newstring.each do |letter|
+      word_to_rev = newstring[array_count] #word currently operating on
+      rev_word= []       #empty array to hold reversed string
+      letterscount = newstring.length
+      newstring.each do |letter|
       rev_word<< word_to_rev[letterscount]
       letterscount -= 1
     end #reversing individual word
     final_string_array << rev_word
+    array_count +=1
   end #string array operation
-  word_to_reverse = newstring
-
   finalstring
 end
