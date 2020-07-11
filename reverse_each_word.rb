d566499d0840153fd2377ea41fd74e0f395e5f26
Denise Henderson <denise@henderson3.us>
2020-07-11 10:51:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,7 @@ def reverse_each_word(string)
   newstring = string.split(" ") #separates string by spaces
   final_string_array = []
   array_count = 0
-  binding.pry
+  #binding.pry
   newstring.each do |word|   #do for each word in array
       word_to_rev = newstring[array_count] #word currently operating on
       rev_word= []       #empty array to hold reversed string
@@ -12,7 +12,7 @@ def reverse_each_word(string)
       newstring.each do |letter|
       rev_word<< word_to_rev[letters_count]
       letters_count -= 1
-      binding.pry
+      #binding.pry
     end #reversing individual word
     final_string_array << rev_word
     array_count +=1
