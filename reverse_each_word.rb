44ddab32a61fe0b673f99317a5c8ccaf556f5c98
Denise Henderson <denise@henderson3.us>
2020-07-11 10:35:52 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,31 +1,24 @@
 require 'pry'
 
 def reverse_each_word(string)
-  newstring = string.split(" ") #separates array by words
+  newstring = string.split(" ") #separates string by words
   arraycount = 0
   newstring.each do |word|   #do for each word in array
-    word_to_reverse=[]       #empty array to hold word string
-    word_to_reverse << newstring[arraycount].split("") #fill array with letters vs string
-    word_to_reverse_array_count = 0
-    word_to_reverse.each do |char|   #count letters to reverse
-      word_to_reverse_array_count += 1
-    end
-    reversed_word = []
-    word_to_reverse.each do |letter|
+    word_to_rev = newstring[arraycount]
+    rev_word=[]       #empty array to hold word string
+    #word_to_reverse << newstring[arraycount].split("") #fill array with letters vs string
+    begin = 1
+    letterscount = newstring.length
+    #word_to_reverse.each do |char|   #count letters to reverse
+    #  word_to_reverse_array_count += 1
+    #end
+    rev_word = []
+    .each do |letter|
     reversed_word<< word_to_reverse[word_to_reverse_array_count]
       word_to_reverse_array_count -= 1
-    end
-  end
+    end #reversing individual word
+  end #string array operation
   word_to_reverse = newstring
-  finalstring = []
-  arraycount=0
-  newstring.each do |char|
-    arraycount+=1
-  end
-  binding.pry
-  newstring.each do |char|
-    finalstring<<string[arraycount]
-    arraycount -= 1
-  end
+  
   finalstring
 end
