d7fdb85b43537f8f6b3649f1555220d487a975ef
Denise Henderson <denise@henderson3.us>
2020-07-11 10:51:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,10 +8,10 @@ def reverse_each_word(string)
   newstring.each do |word|   #do for each word in array
       word_to_rev = newstring[array_count] #word currently operating on
       rev_word= []       #empty array to hold reversed string
-      letterscount = newstring.length
+      letters_count = word_to_rev[length]
       newstring.each do |letter|
-      rev_word<< word_to_rev[letterscount]
-      letterscount -= 1
+      rev_word<< word_to_rev[letters_count]
+      letters_count -= 1
       binding.pry
     end #reversing individual word
     final_string_array << rev_word
